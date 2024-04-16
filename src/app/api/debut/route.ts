fda0a46c87a769f00e086b5a19eae24e87e9165f
k srisri reddy <58304531+srisri332@users.noreply.github.com>
2024-04-16 22:59:25 +0530

    feat: saving data to SB upon submitting preferences

--- src/app/api/debut/route.ts
+++ src/app/api/debut/route.ts
@@ -1,30 +1,49 @@
 import { NextResponse } from "next/server";
 import { generateStudyPlanForUser } from "@/app/utils/openllm";
-
-type ResponseData = {
-  message: string;
-};
+import { supabase } from "@/app/utils/supabaseClient";
+import logger from "@/app/utils/logger";
 
 // TODO: use this route to process the submitted skills, experience and learning pace.
 export function GET(requst: Request, context: any) {
-  const id = context.params.llmID;
-  console.log(context.params.llmID);
   return NextResponse.json({
     message: `process skills, experience and generate questions`,
   });
 }
 
 export async function POST(req: Request) {
+  let body = await req.json();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    logger.error("", `User is not available`);
+    throw new Error(`user is not available`);
+  }
+
   // TODO: add logic to call database to store user details and preferences
+  let { data, error }: any = await supabase
+    .from("preferences")
+    .insert([
+      {
+        experience: body.experience,
+        skills: body.skills,
+        weeks: body.weeks,
+        questions: body.questions,
+        user_id: "messga84baf86c-0c7b-4888-ae0b-e7d55c631767",
+      },
+    ])
+    .select();
 
   // TODO: add complete logic to call openai api to generate questions for user
-  let body = await req.json();
+  let userPreferences = data[0];
   let messgae = await generateStudyPlanForUser(
-    body.promptID,
-    body.experience,
-    body.technologies,
-    body.weeks,
-    body.questions
+    "SINGLE_TECH",
+    userPreferences.experience,
+    userPreferences.skills,
+    userPreferences.weeks,
+    userPreferences.questions
   );
 
   // TODO: store the genearted questiong in database
---
+++ src/app/utils/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+console.log(supabaseUrl);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
