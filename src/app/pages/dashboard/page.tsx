1aed05b1a1417d31583c7f7ba3a558a9b17ec125
k srisri reddy <58304531+srisri332@users.noreply.github.com>
2024-04-20 00:03:50 +0530

    Refactor: refactor dashboard page

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -11,11 +11,27 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import {
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { Underdog } from "next/font/google";
 
 function Dashboard() {
   const [plans, setPlan]: any = useState([]);
+  const [user, setUser] = useState<User | null>(null);
+
+  const supabase = createClientComponentClient();
 
   useEffect(() => {
+    const getUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    getUser();
+
     axios
       .get("http://localhost:3000/api/skills")
       .then((data: any) => {
@@ -30,39 +46,45 @@ function Dashboard() {
 
   return (
     <>
-      <div className='flex justify-around mt-10 '>
-        <span>
-          <p className='text-4xl font-bold'>Hello User!</p>
-          <p className='text-lg'>Pick a study-plan</p>
-        </span>
-        <Avatar>
-          <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </div>
-      <hr />
-      <div className='flex justify-center '>
-        <div className=' w-3/4 min-h-screen  rounded-lg container  grid grid-cols-3'>
-          {/* <div className='container m-auto grid grid-cols-3'> */}
+      {user !== undefined && user != null ? (
+        <>
+          <div className='flex justify-around mt-10 '>
+            <span>
+              <p className='text-4xl font-bold'>Hello User!</p>
+              <p className='text-lg'>Pick a study-plan</p>
+            </span>
+            <Avatar>
+              <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <hr />
+          <div className='flex justify-center '>
+            <div className=' w-3/4 min-h-screen  rounded-lg container  grid grid-cols-3'>
+              {/* <div className='container m-auto grid grid-cols-3'> */}
 
-          {plans.map((plan: any) => {
-            return (
-              <div key={plan}>
-                <Card className='w-[250px] ml-10 '>
-                  <CardHeader>
-                    <CardTitle>{plan}</CardTitle>
-                    <CardDescription>Everybody hates it</CardDescription>
-                  </CardHeader>
-                  {/* <CardContent><p>Card Content</p></CardContent> */}
-                  <CardFooter>
-                    <Button>Practice</Button>
-                  </CardFooter>
-                </Card>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+              {plans.map((plan: any) => {
+                return (
+                  <div key={plan}>
+                    <Card className='w-[250px] ml-10 '>
+                      <CardHeader>
+                        <CardTitle>{plan}</CardTitle>
+                        <CardDescription>Everybody hates it</CardDescription>
+                      </CardHeader>
+                      {/* <CardContent><p>Card Content</p></CardContent> */}
+                      <CardFooter>
+                        <Button>Practice</Button>
+                      </CardFooter>
+                    </Card>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Sanka Naaku. Login Avvu mundu.</p>
+      )}
     </>
   );
 }
