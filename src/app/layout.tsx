a792e9c3330f7dc4e1106a46e770eb3db58d273c
k srisri reddy <58304531+srisri332@users.noreply.github.com>
2024-04-21 16:05:11 +0530

    chore: updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "AI Tutor",
   description: "AI Tutor",
+  icons: {
+    icon: "icon.png",
+  },
 };
 
 export default function RootLayout({
