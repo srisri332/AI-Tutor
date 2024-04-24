58662ba69ef669a7ec04e4905d0d5a3b428ac83a
k srisri reddy <58304531+srisri332@users.noreply.github.com>
2024-04-24 18:35:01 +0530

    chore: code cleanup and stylised planning page

--- src/app/pages/planning/page.tsx
+++ src/app/pages/planning/page.tsx
@@ -11,6 +11,15 @@ import { useRouter } from "next/navigation";
 import { useToast } from "@/components/ui/use-toast";
 import { checkIfUserAlreadyHasPreferences } from "@/app/utils/internal-request";
 
+const BottomGradient = () => {
+  return (
+    <>
+      <span className='group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent' />
+      <span className='group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent' />
+    </>
+  );
+};
+
 export default function Planning() {
   const [value, setValue] = React.useState<Option[]>([]);
   const [experience, setExperience] = useState(3);
@@ -22,9 +31,9 @@ export default function Planning() {
     router.push(path);
   };
 
-  const selectSkills = () => {
+  const selectSkills = (message: string) => {
     toast({
-      title: "Please select at least one skill",
+      title: `${message}`,
       variant: "destructive",
     });
   };
@@ -43,7 +52,12 @@ export default function Planning() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (value?.length === 0) {
-      selectSkills();
+      selectSkills("Please select at least one skill");
+      return;
+    }
+
+    if (value?.length > 6) {
+      selectSkills("Please select skills fewer or equal to 6");
       return;
     }
 
@@ -72,33 +86,33 @@ export default function Planning() {
   const OPTIONS: Option[] = skills;
 
   return (
-    <div className="flex flex-col h-full min-h-screen justify-center">
-      <div className="bg-white dark:bg-black justify-center max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input">
-        <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+    <div className='flex flex-col h-full min-h-screen justify-center dark:bg-dot-white/[0.2] bg-dot-black/[0.2] bg-black'>
+      <div className='bg-white dark:bg-black border border-slate-600 justify-center max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input'>
+        <h2 className='font-bold text-xl text-neutral-800 dark:text-neutral-200'>
           Tell About Yourself
         </h2>
-        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        <p className='text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300'>
           The study plan will be based on the details you enter.
         </p>
 
-        <form className="my-8" onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <Label htmlFor="skills">Skills</Label>
-            <div className="my-4">
+        <form className='my-8' onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <Label htmlFor='skills'>Skills</Label>
+            <div className='my-4'>
               <MultipleSelector
                 defaultOptions={OPTIONS}
-                placeholder="Type something that does not exist in dropdowns..."
+                placeholder='Select upto 6 skills..'
                 value={value}
                 onChange={setValue}
                 creatable
               />
             </div>
           </div>
-          <div className="mb-4">
-            <Label htmlFor="experience">Experience</Label>
-            <div className="flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4">
+          <div className='mb-4'>
+            <Label htmlFor='experience'>Experience</Label>
+            <div className='flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4'>
               <Slider
-                className="mr-2"
+                className='mr-2'
                 defaultValue={[experience]}
                 max={20}
                 step={1}
@@ -107,16 +121,15 @@ export default function Planning() {
                 }}
               />
               <Label
-                htmlFor="experiencevalue"
-                className="w-24"
-              >{`${experience} Years`}</Label>
+                htmlFor='experiencevalue'
+                className='w-24'>{`${experience} Years`}</Label>
             </div>
           </div>
-          <div className="mb-4">
-            <Label htmlFor="duration">Course Duration</Label>
-            <div className="flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4">
+          <div className='mb-4'>
+            <Label htmlFor='duration'>Course Duration</Label>
+            <div className='flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4'>
               <Slider
-                className="mr-2"
+                className='mr-2'
                 defaultValue={[duration]}
                 max={4}
                 step={1}
@@ -125,16 +138,15 @@ export default function Planning() {
                 }}
               />
               <Label
-                htmlFor="durationvalue"
-                className="w-24"
-              >{`${duration} Weeks`}</Label>
+                htmlFor='durationvalue'
+                className='w-24'>{`${duration} Weeks`}</Label>
             </div>
           </div>
-          <div className="mb-8">
-            <Label htmlFor="questions">Questions per week</Label>
-            <div className="flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4">
+          <div className='mb-8'>
+            <Label htmlFor='questions'>Questions per week</Label>
+            <div className='flex flex-col md:flex-row md:space-y-0 md:space-x-2 my-4'>
               <Slider
-                className="mr-2"
+                className='mr-2'
                 defaultValue={[questions]}
                 max={10}
                 step={1}
@@ -142,46 +154,22 @@ export default function Planning() {
                   setQuestions(e[0]);
                 }}
               />
-              <Label htmlFor="questionsvalue" className="w-24">
+              <Label htmlFor='questionsvalue' className='w-24'>
                 {`${questions} Ques.`}
               </Label>
             </div>
           </div>
 
           <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-            type="submit"
-          >
+            className='bg-gradient-to-br relative group/btn dark:bg-white w-full text-black rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
+            type='submit'>
             Let&apos;s Get Started &rarr;
             <BottomGradient />
           </button>
 
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+          <div className='bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full' />
         </form>
       </div>
     </div>
   );
 }
-
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
-
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  );
-};
